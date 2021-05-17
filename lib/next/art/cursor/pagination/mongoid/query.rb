2217a3394d1c29d1dc3a7442cfc946a1b76a3a57
solehudinMQ <solehudinmq@gmail.com>
2021-05-17 23:20:27 +0700

    support for mongoid

---
+++ lib/next/art/cursor/pagination/mongoid/query.rb
@@ -0,0 +1,33 @@
+require 'active_support/concern'
+
+module Next
+  module Art
+    module Cursor
+      module Pagination
+        module Mongoid
+          module Query
+            extend ActiveSupport::Concern
+
+            class_methods do
+              def before(limit=10, *args)
+                limit = 10 if limit.negative?
+                
+                puts "SELF : #{self}"
+                puts "LIMIT : #{limit}"
+                puts "BEFORE : #{args}"
+              end
+
+              def after(limit=10, *args)
+                limit = 10 if limit.negative?
+                
+                puts "SELF : #{self}"
+                puts "LIMIT : #{limit}"
+                puts "AFTER : #{args}"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
